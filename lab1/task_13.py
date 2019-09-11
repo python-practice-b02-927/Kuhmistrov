36202416276acb7d03c06a5c0661079b6adc4e66
WebKooH <54915949+WebKooH@users.noreply.github.com>
2019-09-11 18:46:25 +0500

    Update task_13.py

--- lab1/task_13.py
+++ lab1/task_13.py
@@ -2,32 +2,27 @@
 
 from pyrob.api import *
 
+def cell_top():
+    move_up()
+    fill_cell()
+    move_down()   
+def cell_down():
+    move_down()
+    fill_cell()
+    move_up()
 
 @task
 def task_8_10():
     while not wall_is_on_the_right():
     	if not wall_is_above():
-    		move_up()
-    		fill_cell()
-    		move_down()
+    		cell_top()
     	if not wall_is_beneath():
-    		move_down()
-    		fill_cell()
-    		move_up()
-    	#if wall_is_above() and wall_is_beneath():
-    		#fill_cell()
+    		cell_down()
     	move_right(n=1)
     if not wall_is_above():
-    		move_up()
-    		fill_cell()
-    		move_down()
+    		cell_top()
     if not wall_is_beneath():
-    		move_down()
-    		fill_cell()
-    		move_up()
-    #if wall_is_above() and wall_is_beneath():
-    		#fill_cell()
-
+    		cell_down()
 
 if __name__ == '__main__':
     run_tasks()
