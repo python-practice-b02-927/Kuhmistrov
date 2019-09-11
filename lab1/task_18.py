22d545f78ac9f7eabcb4f8519376fc48e5daca33
WebKooH <54915949+WebKooH@users.noreply.github.com>
2019-09-11 21:11:02 +0500

    Update task_18.py

--- lab1/task_18.py
+++ lab1/task_18.py
@@ -6,23 +6,14 @@ from pyrob.api import *
 @task
 def task_8_28():
     f=False
-    if not wall_is_above():
-    		f=True
-    		move_up()
     while not wall_is_on_the_left() and not f:
     	move_left()
-    	if not wall_is_above():
-    		f=True
-    		move_up()
     if not wall_is_above() and not f:
     		f=True
     		move_up()
     while not wall_is_on_the_right() and not f:
     	move_right()
     	if not wall_is_above():
-    		f=True
-    		move_up()
-    if not wall_is_above() and not f:
     		f=True
     		move_up()
     if f:
