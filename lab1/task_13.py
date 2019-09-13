653387a4f64f2fe88a14043e2aec87d246313cf8
WebKooH <54915949+WebKooH@users.noreply.github.com>
2019-09-13 20:15:39 +0500

    Update task_13.py

--- lab1/task_13.py
+++ lab1/task_13.py
@@ -10,19 +10,18 @@ def cell_down():
     move_down()
     fill_cell()
     move_up()
+def check_fill():
+    if not wall_is_above():
+        cell_top()
+    if not wall_is_beneath():
+        cell_down()
 
 @task
 def task_8_10():
     while not wall_is_on_the_right():
-    	if not wall_is_above():
-    		cell_top()
-    	if not wall_is_beneath():
-    		cell_down()
+    	check_fill()
     	move_right(n=1)
-    if not wall_is_above():
-    		cell_top()
-    if not wall_is_beneath():
-    		cell_down()
+    check_fill()
 
 if __name__ == '__main__':
     run_tasks()
