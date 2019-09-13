4c2a9f7157bb883195f2f04cfe6811e1bdf590d6
WebKooH <54915949+WebKooH@users.noreply.github.com>
2019-09-13 20:40:04 +0500

    Update task_19.py

--- lab1/task_19.py
+++ lab1/task_19.py
@@ -2,28 +2,23 @@
 
 from pyrob.api import *
 
-
+def go_to_the_corner():
+	while not wall_is_above():
+		move_up()
+	while not wall_is_on_the_left():
+		move_left()
 @task
 def task_8_29():
-    f=False
-    if not wall_is_above():
-    		f=True;
+    did_robot_leave=False
     while not wall_is_on_the_left():
     	move_left()
-    	if not wall_is_above():
-    		f=True;
-    while not wall_is_on_the_right() and not f:
+    if not wall_is_above():
+    	go_to_the_corner()
+    	did_robot_leave=True
+    while not wall_is_on_the_right() and not did_robot_leave:
     	move_right()
-    	if not wall_is_above():
-    		f=True;
-    if f:
-    	while not wall_is_above():
-    		move_up()
-    	while not wall_is_on_the_left():
-    		move_left()
-    else:
-    	while not wall_is_on_the_right():
-    		move_right()
+    if not wall_is_above() and not did_robot_leave:
+    	go_to_the_corner()
 
 
 
