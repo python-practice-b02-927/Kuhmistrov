388f84d9584a40f97d9b2d4ed046d7073dff1953
WebKooH <54915949+WebKooH@users.noreply.github.com>
2019-09-16 20:28:28 +0500

    Update task_30.py

--- lab1/task_30.py
+++ lab1/task_30.py
@@ -13,23 +13,23 @@ def task_9_3():
     a+=1
     
     
-    for i in range(1,a,1):
-    	for j in range(1,a,1):
-    		if (not i==j) and (not i+j==a+1):
+    for i in range(a-1):
+    	for j in range(a-1):
+    		if (not i==j) and (not i+j==a-1):
     			fill_cell()
     		move_right()
     	j+=1
-    	if (not i==j) and (not i+j==a+1):
+    	if (not i==j) and (not i+j==a-1):
     		fill_cell()
     	move_left(n=a-1)
     	move_down()
-    i=a	
-    for j in range(1,a,1):
-    	if (not i==j) and (not i+j==a+1):
+    i=a-1
+    for j in range(a-1):
+    	if (not i==j) and (not i+j==a-1):
     		fill_cell()
     	move_right()
     j+=1
-    if (not i==j) and (not i+j==a+1):
+    if (not i==j) and (not i+j==a-1):
     	fill_cell()
     move_left(n=a-1)
 
