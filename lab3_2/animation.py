1067399f38d92c6d65750080e3c0df1c471b869f
WebKooH <webkooh@gmail.com>
2019-10-01 03:21:57 +0500

    add acceleration of gravity as const add part of recount_acceleration() function

--- lab3_2/animation.py
+++ lab3_2/animation.py
@@ -3,6 +3,9 @@ import graphics as gr
 SIZE_X = 1080
 SIZE_Y = 720
 
+#acceleration of gravity
+g=5
+
 window = gr.GraphWin("Animation_Win", SIZE_X, SIZE_Y)
 
 def add_coords(point_1, point_2):
@@ -23,5 +26,8 @@ def recount_coords(coords, velocity):
 def recount_velocity(velocity, acceleration):
     return add(velocity, acceleration)
 
+def recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_inertia, mass):
+    radius_vector = sub(pendulum_coords, center_coords)
+    distance = absolute_value(radius_vector)
 
 main()
