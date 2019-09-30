54200a0fa9341b2b05fdeeef867824d9cb0c283b
WebKooH <webkooh@gmail.com>
2019-10-01 02:54:14 +0500

    add add_coords() function

--- lab3_2/animation.py
+++ lab3_2/animation.py
@@ -5,4 +5,11 @@ SIZE_Y = 720
 
 window = gr.GraphWin("Animation_Win", SIZE_X, SIZE_Y)
 
+def add_coords(point_1, point_2):
+    total = gr.Point(point_1.x + point_2.x, point_1.y + point_2.y)
+    return total
+
+
+
+
 main()
