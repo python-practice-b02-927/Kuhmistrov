47c1a4cfbe6d274d93a37a2427e21ec5492c3388
WebKooH <webkooh@gmail.com>
2019-10-01 04:37:51 +0500

    WARNING This commit is not right, programm doesnt work. Were fixed a lot of mistakes and bugs. Keeping on working

--- lab3_2/animation.py
+++ lab3_2/animation.py
@@ -5,7 +5,7 @@ SIZE_X = 1080
 SIZE_Y = 720
 
 #acceleration of gravity
-g=5
+g=10
 
 window = gr.GraphWin("Animation_Win", SIZE_X, SIZE_Y)
 
@@ -26,13 +26,13 @@ def absolute_value(vector):
     return result
 
 def recount_coords(coords, velocity):
-    return add(coords,velocity)
+    return add_coords(coords,velocity)
 
 def recount_velocity(velocity, acceleration):
-    return add(velocity, acceleration)
+    return add_coords(velocity, acceleration)
 
 def recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_inertia, mass):
-    radius_vector = sub(pendulum_coords, center_coords)
+    radius_vector = sub_coords(pendulum_coords, center_coords)
     distance = absolute_value(radius_vector)
     absolute_velocity = absolute_value(velocity)
     angular_acceleration = (g * mass * radius_vector.x) / (distance * moment_of_inertia)
@@ -42,10 +42,10 @@ def recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_ine
 
     tangential_acceleration = gr.Point(angular_acceleration * (-1) * radius_vector.y, angular_acceleration * radius_vector.x)
 
-    return add(normal_acceleration,tangential_acceleration)
+    return add_coords(normal_acceleration,tangential_acceleration)
 
 def recount_pendulum_angle(pendulum_coords, center_coords):
-    radius_vector = sub(pendulum_coords, center_coords)
+    radius_vector = sub_coords(pendulum_coords, center_coords)
     distance = absolute_value(radius_vector)
     return radius_vector.x/distance
 
@@ -53,20 +53,23 @@ def drawing_pendulum(pendulum_coords, pendulum_angle, pendulum_side):
     c=pendulum_coords
     a=pendulum_angle
     s=pendulum_side / 1.41
-    pendulum = gr.Polygon(gr.Point(c.x - s*math.sin(math.pi/4 - a)),gr.Point(c.y - s*math.cos(math.pi/4 - a)),
-               gr.Point(c.x + s*math.cos(math.pi/4 - a)),gr.Point(c.y - s*math.sin(math.pi/4 - a)),
-               gr.Point(c.x + s*math.sin(math.pi/4 - a)),gr.Point(c.y + s*math.cos(math.pi/4 - a)),
-               gr.Point(c.x - s*math.cos(math.pi/4 - a)),gr.Point(c.y + s*math.sin(math.pi/4 - a)))
+    pendulum = gr.Polygon(gr.Point((c.x - s*math.sin(math.pi/4 - a)),(c.y - s*math.cos(math.pi/4 - a))),
+               gr.Point((c.x + s*math.cos(math.pi/4 - a)),(c.y - s*math.sin(math.pi/4 - a))),
+               gr.Point((c.x + s*math.sin(math.pi/4 - a)),(c.y + s*math.cos(math.pi/4 - a))),
+               gr.Point((c.x - s*math.cos(math.pi/4 - a)),(c.y + s*math.sin(math.pi/4 - a))))
+    #pendulum = gr.Circle(pendulum_coords, pendulum_side)
     pendulum.setFill('white')
-    pendulum.undraw(window)
+    pendulum.undraw()
 
 
-pendulum_coords = gr.Point(540, 540)
+#main body begins there ->
+
+pendulum_coords = gr.Point(540, 480)
 center_coords = gr.Point(540, 360)
-velocity = (10, 0)
-pendulum_side = 100
+velocity = gr.Point(5, 0)
+pendulum_side = 60
 distance = absolute_value(sub_coords(pendulum_coords,center_coords))
-mass = 500
+mass = 200
 moment_of_inertia = mass * (pendulum_side**2/12 + distance**2)
 
 kernel = gr.Line(center_coords,pendulum_coords)
@@ -74,5 +77,20 @@ kernel.setFill('white')
 kernel.setWidth(3)
 kernel.draw(window)
 
+pendulum = gr.Rectangle(gr.Point(480,540),gr.Point(600,420))
+pendulum.setFill('white')
+pendulum.draw(window)
+
 while True:
-    pass
+        pendulum_coords = recount_coords(pendulum_coords, velocity)
+        kernel.undraw()
+        kernel = gr.Line(pendulum_coords, center_coords)
+        kernel.setFill('white')
+        kernel.setWidth(3)
+        kernel.draw(window)
+
+        drawing_pendulum(pendulum_coords, recount_pendulum_angle(pendulum_coords, center_coords), pendulum_side)
+        acceleration = recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_inertia, mass)
+        velocity = recount_velocity(velocity, acceleration)
+
+        gr.time.sleep(0.02)    
