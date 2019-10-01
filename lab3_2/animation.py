c220157d558da458febefbf588e83665bc05288e
WebKooH <webkooh@gmail.com>
2019-10-01 11:48:56 +0500

    BEWARE OF huge update of programm in this version
    add probability to drawing circle_pendulum
    drawing different pendulums distinguished in separate functions
    main body changed according to last changes in architecture

--- lab3_2/animation.py
+++ lab3_2/animation.py
@@ -51,46 +51,105 @@ def recount_pendulum_angle(pendulum_coords, center_coords):
 
 #main body begins there ->
 
-pendulum_coords = gr.Point(540, 480)
-center_coords = gr.Point(540, 100)
-velocity = gr.Point(10, 0)
-pendulum_side = 60
-distance = absolute_value(sub_coords(pendulum_coords,center_coords))
-mass = 500
-moment_of_inertia = mass * (pendulum_side**2/12 + distance**2)
+def square_pendulum():
+    pendulum_coords = gr.Point(540, 480)
+    center_coords = gr.Point(540, 100)
+    velocity = gr.Point(10, 0)
+    pendulum_side = 60
+    distance = absolute_value(sub_coords(pendulum_coords,center_coords))
+    mass = 500
+    moment_of_inertia = mass * (pendulum_side**2/12 + distance**2)
 
-kernel = gr.Line(center_coords,pendulum_coords)
-kernel.setFill('white')
-kernel.setWidth(3)
-kernel.draw(window)
+    kernel = gr.Line(center_coords,pendulum_coords)
+    kernel.setFill('white')
+    kernel.setWidth(3)
+    kernel.draw(window)
 
-pendulum = gr.Rectangle(gr.Point(480,540),gr.Point(600,420))
-pendulum.setFill('white')
-pendulum.draw(window)
+    pendulum = gr.Rectangle(gr.Point(480,540),gr.Point(600,420))
+    pendulum.setFill('white')
+    pendulum.draw(window)
 
-while True:
-        pendulum_coords = recount_coords(pendulum_coords, velocity)
+    while True:
+            pendulum_coords = recount_coords(pendulum_coords, velocity)
 #i have no idea how to redraw figure in another function instead of one, where it was named, so i leave redrawing
 #pendulum and kernel in the main body        
-        kernel.undraw()
-        kernel = gr.Line(pendulum_coords, center_coords)
-        kernel.setFill('white')
-        kernel.setWidth(3)
-        kernel.draw(window)
+            kernel.undraw()
+            kernel = gr.Line(pendulum_coords, center_coords)
+            kernel.setFill('white')
+            kernel.setWidth(3)
+            kernel.draw(window)
         
-        pendulum.undraw()
-        c=pendulum_coords
-        a=recount_pendulum_angle(pendulum_coords, center_coords)
-        s=pendulum_side / 1.41
-        pendulum = gr.Polygon(gr.Point((c.x - s*math.sin(math.pi/4 - a)),(c.y - s*math.cos(math.pi/4 - a))),
-                   gr.Point((c.x + s*math.cos(math.pi/4 - a)),(c.y - s*math.sin(math.pi/4 - a))),
-                   gr.Point((c.x + s*math.sin(math.pi/4 - a)),(c.y + s*math.cos(math.pi/4 - a))),
-                   gr.Point((c.x - s*math.cos(math.pi/4 - a)),(c.y + s*math.sin(math.pi/4 - a))))
-        pendulum.setFill('white')
-        pendulum.draw(window)
+            pendulum.undraw()
+            c=pendulum_coords
+            a=recount_pendulum_angle(pendulum_coords, center_coords)
+            s=pendulum_side / 1.41
+            pendulum = gr.Polygon(gr.Point((c.x - s*math.sin(math.pi/4 - a)),(c.y - s*math.cos(math.pi/4 - a))),
+                       gr.Point((c.x + s*math.cos(math.pi/4 - a)),(c.y - s*math.sin(math.pi/4 - a))),
+                       gr.Point((c.x + s*math.sin(math.pi/4 - a)),(c.y + s*math.cos(math.pi/4 - a))),
+                       gr.Point((c.x - s*math.cos(math.pi/4 - a)),(c.y + s*math.sin(math.pi/4 - a))))
+            pendulum.setFill('white')
+            pendulum.draw(window)
         
 
-        acceleration = recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_inertia, mass)
-        velocity = recount_velocity(velocity, acceleration)
+            acceleration = recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_inertia, mass)
+            velocity = recount_velocity(velocity, acceleration)
 
-        gr.time.sleep(0.02)    
+            gr.time.sleep(0.02)
+
+def circle_pendulum():
+    pendulum_coords = gr.Point(540, 480)
+    center_coords = gr.Point(540, 100)
+    velocity = gr.Point(10, 0)
+    pendulum_side = 60
+    distance = absolute_value(sub_coords(pendulum_coords,center_coords))
+    mass = 500
+    moment_of_inertia = mass * (0.05*pendulum_side**2 + distance**2)
+
+    kernel = gr.Line(center_coords,pendulum_coords)
+    kernel.setFill('white')
+    kernel.setWidth(3)
+    kernel.draw(window)
+
+    pendulum1 = gr.Circle(pendulum_coords, pendulum_side)
+    pendulum1.setFill('white')
+    pendulum1.draw(window)
+
+    while True:
+            pendulum_coords = recount_coords(pendulum_coords, velocity)
+#i have no idea how to redraw figure in another function instead of one, where it was named, so i leave redrawing
+#pendulum and kernel in the main body        
+            kernel.undraw()
+            kernel = gr.Line(pendulum_coords, center_coords)
+            kernel.setFill('white')
+            kernel.setWidth(3)
+            kernel.draw(window)
+        
+            pendulum1.undraw()
+            #c=pendulum_coords
+            #a=recount_pendulum_angle(pendulum_coords, center_coords)
+            #s=pendulum_side / 1.41
+            #pendulum = gr.Polygon(gr.Point((c.x - s*math.sin(math.pi/4 - a)),(c.y - s*math.cos(math.pi/4 - a))),
+            #           gr.Point((c.x + s*math.cos(math.pi/4 - a)),(c.y - s*math.sin(math.pi/4 - a))),
+            #           gr.Point((c.x + s*math.sin(math.pi/4 - a)),(c.y + s*math.cos(math.pi/4 - a))),
+            #           gr.Point((c.x - s*math.cos(math.pi/4 - a)),(c.y + s*math.sin(math.pi/4 - a))))
+            pendulum1 = gr.Circle(pendulum_coords, pendulum_side)
+            pendulum1.setFill('white')
+            pendulum1.draw(window)
+        
+
+            acceleration = recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_inertia, mass)
+            velocity = recount_velocity(velocity, acceleration)
+
+            gr.time.sleep(0.02)
+
+#main part of body ->
+print('Choose one of pendulums. Insert 1 if square_pendulum. Insert 2 if circle_pendulum')
+choice = int(input())
+
+if choice == 1:
+    square_pendulum()
+    print('OK. Change window to see the pendulum')
+
+if choice == 2:
+    circle_pendulum()
+    print('OK. Change window to see the pendulum')
