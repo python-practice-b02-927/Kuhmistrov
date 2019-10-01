9f7878f0d1b08e2f984f62af50eed5194e41dfd7
WebKooH <webkooh@gmail.com>
2019-10-01 11:57:16 +0500

    add some 'space' - symbols according to a codestyle

--- lab3_2/animation.py
+++ lab3_2/animation.py
@@ -37,7 +37,7 @@ def recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_ine
     absolute_velocity = absolute_value(velocity)
     angular_acceleration = (g * mass * radius_vector.x) / ( moment_of_inertia)
 
-    normal_acceleration = gr.Point(((-1) * radius_vector.x * absolute_velocity**2)/(distance**2),
+    normal_acceleration = gr.Point(((-1) * radius_vector.x * absolute_velocity**2) / (distance**2),
     ((-1) * radius_vector.y * absolute_velocity**2)/(distance**2))
 
     tangential_acceleration = gr.Point(angular_acceleration * (-1) * radius_vector.y, angular_acceleration * radius_vector.x)
@@ -47,7 +47,7 @@ def recount_acceleration(pendulum_coords, center_coords, velocity, moment_of_ine
 def recount_pendulum_angle(pendulum_coords, center_coords):
     radius_vector = sub_coords(pendulum_coords, center_coords)
     distance = absolute_value(radius_vector)
-    return (-1)*radius_vector.x/distance   
+    return (-1) * radius_vector.x / distance   
 
 
 def square_pendulum():
@@ -55,16 +55,16 @@ def square_pendulum():
     center_coords = gr.Point(540, 100)
     velocity = gr.Point(10, 0)
     pendulum_side = 60
-    distance = absolute_value(sub_coords(pendulum_coords,center_coords))
+    distance = absolute_value(sub_coords(pendulum_coords, center_coords))
     mass = 500
     moment_of_inertia = mass * (pendulum_side**2/12 + distance**2)
 
-    kernel = gr.Line(center_coords,pendulum_coords)
+    kernel = gr.Line(center_coords, pendulum_coords)
     kernel.setFill('white')
     kernel.setWidth(3)
     kernel.draw(window)
 
-    pendulum = gr.Rectangle(gr.Point(480,540),gr.Point(600,420))
+    pendulum = gr.Rectangle(gr.Point(480, 540), gr.Point(600, 420))
     pendulum.setFill('white')
     pendulum.draw(window)
 
@@ -82,10 +82,10 @@ def square_pendulum():
             c=pendulum_coords
             a=recount_pendulum_angle(pendulum_coords, center_coords)
             s=pendulum_side / 1.41
-            pendulum = gr.Polygon(gr.Point((c.x - s*math.sin(math.pi/4 - a)),(c.y - s*math.cos(math.pi/4 - a))),
-                       gr.Point((c.x + s*math.cos(math.pi/4 - a)),(c.y - s*math.sin(math.pi/4 - a))),
-                       gr.Point((c.x + s*math.sin(math.pi/4 - a)),(c.y + s*math.cos(math.pi/4 - a))),
-                       gr.Point((c.x - s*math.cos(math.pi/4 - a)),(c.y + s*math.sin(math.pi/4 - a))))
+            pendulum = gr.Polygon(gr.Point((c.x - s*math.sin(math.pi/4 - a)), (c.y - s*math.cos(math.pi/4 - a))),
+                       gr.Point((c.x + s*math.cos(math.pi/4 - a)), (c.y - s*math.sin(math.pi/4 - a))),
+                       gr.Point((c.x + s*math.sin(math.pi/4 - a)), (c.y + s*math.cos(math.pi/4 - a))),
+                       gr.Point((c.x - s*math.cos(math.pi/4 - a)), (c.y + s*math.sin(math.pi/4 - a))))
             pendulum.setFill('white')
             pendulum.draw(window)
         
@@ -100,11 +100,11 @@ def circle_pendulum():
     center_coords = gr.Point(540, 100)
     velocity = gr.Point(10, 0)
     pendulum_side = 60
-    distance = absolute_value(sub_coords(pendulum_coords,center_coords))
+    distance = absolute_value(sub_coords(pendulum_coords, center_coords))
     mass = 500
     moment_of_inertia = mass * (0.05*pendulum_side**2 + distance**2)
 
-    kernel = gr.Line(center_coords,pendulum_coords)
+    kernel = gr.Line(center_coords, pendulum_coords)
     kernel.setFill('white')
     kernel.setWidth(3)
     kernel.draw(window)
